16d5c3f0e49156cddf22605773f64888fa4c3ea6
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-03-06 21:54:58 -0300

    Create and import PostItem

---
+++ src/components/PostItem/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { string } from "prop-types"
+
+import * as S from "./styled"
+
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
+    <S.PostItemWrapper>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
+      <S.PostItemInfo>
+        <S.PostItemDate>{timeToRead}</S.PostItemDate>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
+      </S.PostItemInfo>
+    </S.PostItemWrapper>
+  </S.PostItemLink>
+)
+
+PostItem.propTypes = {
+  slug: string.isRequired,
+  background: string,
+  category: string.isRequired,
+  date: string.isRequired,
+  timeToRead: string.isRequired,
+  title: string.isRequired,
+  description: string.isRequired,
+}
+
+export default PostItem
